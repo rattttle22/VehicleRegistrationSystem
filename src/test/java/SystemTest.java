a15d01fd42af2242f6f6c696641be5a217b13f5e
Saurav Ansuman <sarvkan@gmail.com>
2026-03-29 23:17:14 +0530

    Update test cases to use Saurav Ansuman

--- src/test/java/SystemTest.java
+++ src/test/java/SystemTest.java
@@ -13,14 +13,14 @@ public class SystemTest {
     @Test
     public void testAuthentication() {
         LoginSignUp auth = new LoginSignUp();
-        auth.signUp("Advait Bhore", "advait@test.com", "secure123");
-        assertTrue(auth.login("advait@test.com", "secure123"));
+        auth.signUp("Saurav Ansuman", "saurav@test.com", "secure123");
+        assertTrue(auth.login("saurav@test.com", "secure123"));
     }
 
     @Test
     public void testVehicleRegistration() {
         VehicleRegistration reg = new VehicleRegistration();
-        reg.registerVehicle("MH-15-AB-1234", "Advait Bhore");
+        reg.registerVehicle("MH-15-AB-1234", "Saurav Ansuman");
         assertTrue(reg.getDetailsAndVerifyDetails());
     }
 
@@ -28,7 +28,7 @@ public class SystemTest {
     public void testLicenseEligibility() {
         VehicleLicensing license = new VehicleLicensing();
         license.age = 24;
-        license.nameAddress = "Advait Bhore";
+        license.nameAddress = "Saurav Ansuman";
         assertTrue(license.provideLicenseToDrive());
     }
 
@@ -42,7 +42,7 @@ public class SystemTest {
     @Test
     public void testExaminationPass() {
         Examination exam = new Examination();
-        exam.name = "Advait Bhore";
+        exam.name = "Saurav Ansuman";
         exam.figureOutEligibleCandidates(85);
         assertEquals("Approved_Examiner", exam.signature);
     }
